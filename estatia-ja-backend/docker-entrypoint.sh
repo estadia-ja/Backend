76300323c365d614c61f5e45c45babbfbe6013c1
pkbceira03 <cabeceira2003@gmail.com>
2025-11-14 09:43:52 -0300

    fix: limpa o script do docker

--- estatia-ja-backend/docker-entrypoint.sh
+++ estatia-ja-backend/docker-entrypoint.sh
@@ -1,26 +1,19 @@
-#!/bin/sh
-
-# Aborta o script se qualquer comando falhar
 set -e
 
-echo "📦 Verificando dependências do projeto..."
+echo "Verificando dependências do projeto..."
 
-# Se o diretório node_modules não existir, instala as dependências
 if [ ! -d "node_modules" ]; then
-  echo "📥 Instalando dependências..."
+  echo "Instalando dependências..."
   npm install
 else
-  echo "✅ Dependências já instaladas."
+  echo "Dependências já instaladas."
 fi
 
-echo "🗃️ Executando migrações do banco de dados..."
-# Executa as migrações
+echo "Executando migrações do banco de dados..."
 npx prisma migrate deploy
 
-echo "⚙️ Gerando Prisma Client..."
-# Gera o cliente Prisma (boa prática após migrações)
+echo "Gerando Prisma Client..."
 npx prisma generate
 
-echo "🚀 Iniciando a aplicação..."
-# Substitui o shell atual pelo processo do Node (importante para sinais do Docker)
+echo "Iniciando a aplicação..."
 exec "$@"
